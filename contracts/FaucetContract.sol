13cf7c6f0a7f200107c6f1a29a22ef75ef4838d1
Tamim Ehsas <tamimehsas99@gmail.com>
2021-11-23 18:07:05 +0430

    Add the addfunds method to the faucet contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,7 +4,5 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
     receive() external payable {}
 
-    function addFunds() external payable {
-        
-    }
+    function addFunds() external payable {}
 }
