e120ed7081f14d630241216f1edf02c54234273e
Tamim Ehsas <tamimehsas99@gmail.com>
2021-11-27 20:38:26 +0430

    add the getallfunders method signature

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -12,9 +12,10 @@ contract Faucet {
         funders[index] = msg.sender;
     }
 
-    // function getAllFunders() public view returns (address[] memory){
-    //     return funders;
-    // }
+    function getAllFunders() external view returns (address[] memory){
+        
+    }
+
     function getFunderAtIndex(uint8 index) external view returns(address){
         return funders[index];
     }
