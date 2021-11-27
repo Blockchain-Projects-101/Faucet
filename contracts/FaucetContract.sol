c98204f7b947bc0d7a90680b4792f0f44dd1ca9d
Tamim Ehsas <tamimehsas99@gmail.com>
2021-11-27 20:50:32 +0430

    Update the addFunds method to avoid duplication

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,24 +3,27 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     uint256 public numOfFunders;
-    mapping(uint256 => address) public funders;
+    mapping(address => bool) public funders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint256 index = numOfFunders++;
-        funders[index] = msg.sender;
-    }
-
-    function getAllFunders() external view returns (address[] memory) {
-        address[] memory _funders = new address[](numOfFunders);
-        for (uint256 i = 0; i < numOfFunders; i++) {
-            _funders[i] = funders[i];
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            numOfFunders++;
+            funders[funder] = true;
         }
-        return _funders;
     }
 
-    function getFunderAtIndex(uint8 index) external view returns (address) {
-        return funders[index];
-    }
+    // function getAllFunders() external view returns (address[] memory) {
+    //     address[] memory _funders = new address[](numOfFunders);
+    //     for (uint256 i = 0; i < numOfFunders; i++) {
+    //         _funders[i] = funders[i];
+    //     }
+    //     return _funders;
+    // }
+
+    // function getFunderAtIndex(uint8 index) external view returns (address) {
+    //     return funders[index];
+    // }
 }
