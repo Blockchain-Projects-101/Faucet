c3a43cb5791b50ff2bffed87f377d3e241476b9e
Tamim Ehsas <tamimehsas99@gmail.com>
2021-11-27 20:41:37 +0430

    Add the logic to get All Funders method

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,21 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    uint public numOfFunders;
-    mapping(uint=>address) public funders;
+    uint256 public numOfFunders;
+    mapping(uint256 => address) public funders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numOfFunders++;
+        uint256 index = numOfFunders++;
         funders[index] = msg.sender;
     }
 
-    function getAllFunders() external view returns (address[] memory){
-        
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+        for (uint256 i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+        return _funders;
     }
 
-    function getFunderAtIndex(uint8 index) external view returns(address){
+    function getFunderAtIndex(uint8 index) external view returns (address) {
         return funders[index];
     }
-} 
+}
