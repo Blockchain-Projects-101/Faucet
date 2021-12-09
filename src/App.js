751d501dadf43792e0486dc12f42d3c20b22cc05
Tamim Ehsas <tamimehsas99@gmail.com>
2021-12-09 11:54:30 +0430

    Add the useEffect hook to load the web3 library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import {
+  useEffect
+} from "react";
 
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      /*
+      1. with meta mask we have access to window.ethereum and window.web3
+      2.metamask injects a global API to the website
+      3.this API allows websites to request users, accounts,read data from blockchain,sign messages 
+      and transactions.
+
+      */
+      console.log(window.ethereum);
+      console.log(window.web3);
+    }
+    loadProvider()
+  }, []);
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
         <div className="balance-view is-size-2">
-          Current Balance <strong>10</strong> ETH 
+          Current Balance <strong>10</strong> ETH
         </div>
         <button className="btn mr-2">Donate</button>
         <button className="btn">Withdraw</button>
