bb060266bdf9abff972808f101406d7bb7e7a243
Tamim Ehsas <tamimehsas99@gmail.com>
2021-12-09 21:44:54 +0430

    Refactor: open the metamask popup on button click

--- src/App.js
+++ src/App.js
@@ -40,26 +40,31 @@ function App() {
     <div className="faucet-wrapper">
       <div className="faucet">
         <div className="is-flex">
-        <span>
-          <strong className="mr-2">Account:</strong>
-        </span>
-        <h1>{account ?
-         account :
-          <button className="button ml-2 is-small">Connect wallet</button>
+          <span>
+            <strong className="mr-2">Account:</strong>
+          </span>
+          <h1>{account ?
+            account :
+            <button className="button ml-2 is-small" 
+            onClick={()=>{
+                web3Api.provider.request({method:"eth_requestAccounts"})
+              }
+              }
+              >Connect wallet</button>
           }
-          </h1>
-          </div>
-        <div className="balance-view is-size-2 mb-4">
-          Current Balance <strong>10</strong> ETH
-        </div>
-        <button className="button is-link mr-2">Donate</button>
-        <button className="button is-primary">Withdraw</button>
-        {/* <button className="btn" onClick={async () => {
+        </h1>
+      </div>
+      <div className="balance-view is-size-2 mb-4">
+        Current Balance <strong>10</strong> ETH
+      </div>
+      <button className="button is-link mr-2">Donate</button>
+      <button className="button is-primary">Withdraw</button>
+      {/* <button className="btn" onClick={async () => {
           const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
           console.log(accounts);
         }}>Enable Ethereum</button> */}
-      </div>
     </div>
+    </div >
 
   );
 }
