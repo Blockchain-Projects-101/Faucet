4c6b3ae5be27c27cbfe3db31fb8c5cdcb16ccfa3
Tamim Ehsas <tamimehsas99@gmail.com>
2021-12-09 20:59:13 +0430

    Display the current connected accounts address on the page

--- src/App.js
+++ src/App.js
@@ -35,15 +35,17 @@ function App() {
   useEffect(() => {
     const getAccount = async () => {
 
-      const accounts = await web3Api.web3.ethereum.getAccounts();
+      const accounts = await web3Api.web3.eth.getAccounts();
       setAccount(accounts[0]);
     }
-    web3Api.web3 && getAccount()
+    web3Api.web3 && getAccount();
   }, [web3Api.web3]);
 
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
+        <span>Account</span>
+        <h1>{account ? account : "Not connected"}</h1>
         <div className="balance-view is-size-2">
           Current Balance <strong>10</strong> ETH
         </div>
