914a64f1b0321746529f05d71daa885165790258
Tamim Ehsas <tamimehsas99@gmail.com>
2021-12-10 12:45:26 +0430

    import the truffle/contract to loadcontract util and use it

--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,9 +1,7 @@
-
+import contract from '@truffle/contract';
 
 export const loadContract = async (name) => {
     const res = await fetch(`/contracts/${name}.json`);
     const artifact = res.json();
-    return {
-        contract: artifact
-    }
+    return contract(artifact);
 }
