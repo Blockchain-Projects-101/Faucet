226bc9c2d38a483d3fcf471e0966a7f99f68221c
Tamim Ehsas <tamimehsas99@gmail.com>
2021-12-10 13:07:05 +0430

    Add the provider to load contract js util

--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,7 +1,9 @@
 import contract from '@truffle/contract';
 
-export const loadContract = async (name) => {
+export const loadContract = async (name, provider) => {
     const res = await fetch(`/contracts/${name}.json`);
     const artifact = res.json();
-    return contract(artifact);
+    const _contract = contract(artifact);
+    _contract.setProvider(provider);
+    return _contract;
 }
